637ec920814d3c74dd5a1e45e70819af54834dff
Justin Bellero <jpbellero@gmail.com>
2019-11-15 15:25:53 -0500

    [UPDATE]: create base layout to extend

---
+++ laravel/resources/views/base.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>HELLO</title>
+        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+        <link href="css/base.css" rel="stylesheet">
+    </head>
+
+    <body class="bg-gray-200">
+
+        @yield('content')
+
+    </body>
+</html>
