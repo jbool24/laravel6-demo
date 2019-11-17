23d455c09d3cfaa4c8c769f5ab53a385ff420132
Justin Bellero <jpbellero@gmail.com>
2019-11-17 17:11:00 -0500

    [UPDATE]: laravel container build configs

---
+++ containers/laravel/bootstrap.sh
@@ -0,0 +1,12 @@
+#!usr/bin/env sh
+
+# install node_modules with yarn if they aren't installed
+if [ ! -d "/var/www/laravel/node_modules" ]; then
+  yarn install;
+fi
+
+# install vendor files with composer if they aren't installed
+if [ ! -d "/var/www/laravel/vendor" ]; then
+  composer install;
+fi
+
