f8cd453ca7d9d256a4b3a04514f592a34abaf9fa
Justin Bellero <jpbellero@gmail.com>
2019-11-18 12:34:32 -0500

    [UPDATE]: user joke event

--- laravel/app/Events/UserJoked.php
+++ laravel/app/Events/UserJoked.php
@@ -2,14 +2,31 @@
 
 namespace App\Events;
 
+use App\User;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
 
-
-class UserJoked implements ShouldBroadcast
+class UserJoked implements ShouldBroadcastNow
 {
+    use SerializesModels;
+    use Dispatchable;
+    use InteractsWithSockets;
+
+    public $user;
+    public $joke;
+
+    public function __construct($user, $joke) {
+        $this->user = $user;
+        $this->joke = $joke;
+    }
+
+    public function broadcastWith() {
+        return ['username' => $this->user->name, 'joke' => $this->joke];
+    }
+
     public function broadcastOn() {
         return new Channel('jokes');
     }
